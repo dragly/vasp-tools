5ba36441adff553830c27177f2e49bffd13ea8a3
Milad Hobbi Mobarhan <miladh@fys.uio.no>
2012-09-24 18:30:44 +0200

    ...

--- plotgenerator.py
+++ plotgenerator.py
@@ -6,14 +6,19 @@ import subprocess
 rootdir = argv[1]
 
 
-
 #open different folders: sc_runs->0000->
 for x in os.listdir(rootdir):
-  print rootdir + "/" + x
+  poscar = []
+  f = open(rootdir + "/" + x + "/POSCAR")
+  for line in f:
+      poscar.append(line)
+  f.close()
+  
   output = subprocess.check_output(["toten", "OUTCAR"], cwd=rootdir + "/" + x)
   lines = output.split("\n") 
-  value = lines[1].split("\t")[1]
-  print value
+  value = float(lines[1].split("\t")[1])
+  
+  plot(float(poscar[1]),value)
   
 #Read the energy
   
